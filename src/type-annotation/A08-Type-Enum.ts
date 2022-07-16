1074f44a6988960aec1f8f0996fe931052676f51
carlosbandelliv@gmail.com <carlosbandelliv@gmail.com>
2022-07-16 00:26:10 -0300

    Create A08-Type-Enum.ts

---
+++ src/type-annotation/A08-Type-Enum.ts
@@ -0,0 +1,9 @@
+//Suponha que eu tenho que colocar o nome das cores e codigo nas cores para isso serve o enum
+enum Cores {
+  Vermelho,
+  Azul,
+  Amarelo,
+}
+
+console.log(Cores);
+console.table(Cores);
